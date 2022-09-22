5622b4b599c9a6f6616044f8753faafc19a29d7e
lehongson1998 <lhongson2909@gmail.com>
2022-09-22 13:33:12 +0700

    up 2

--- ss2_angular/exercise/angular-calculator-app/src/app/color/color.component.ts
+++ ss2_angular/exercise/angular-calculator-app/src/app/color/color.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Color} from "../../model/Color";
 
 @Component({
   selector: 'app-color',
@@ -6,10 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./color.component.css']
 })
 export class ColorComponent implements OnInit {
+  color: Color = {
+    id:1,
+    color:"#00ff86"
+  }
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  changeColor(target: any) {
+    this.color.color = target.value;
+  }
 }
